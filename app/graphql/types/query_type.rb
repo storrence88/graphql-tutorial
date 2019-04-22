120a074291eb16b0c7fe88c40b8900ab069a6b8d
Steven Torrence <storrence88@gmail.com>
2019-04-21 20:13:10 -0500

    Update query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,15 +1,5 @@
 module Types
-  class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # queries are just represented as fields
-    # `all_links` is automatically camelcased to `allLinks`
-    field :all_links, [LinkType], null: false
-
-    # this method is invoked, when `all_link` fields is being resolved
-    def all_links
-      Link.all
-    end
+  class QueryType < BaseObject
+    field :all_links, function: Resolvers::LinksSearch
   end
 end
