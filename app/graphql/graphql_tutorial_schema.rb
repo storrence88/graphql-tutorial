f2aa7ff2e3392447f03c055ebaf5c4a6631bc582
Steven Torrence <storrence88@gmail.com>
2019-04-22 21:30:36 -0500

    GraphQL

--- app/graphql/graphql_tutorial_schema.rb
+++ app/graphql/graphql_tutorial_schema.rb
@@ -1,4 +1,7 @@
 class GraphqlTutorialSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
+  type Query {
+    allLinks(filter: LinkFilter, skip: Int, first: Int): [Link!]!
+  }
 end
